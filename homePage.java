6273ded3e9f7fd1865783df8b80e32a90b7200ba
Pulkit Madaan <pulkit16257@iiitd.ac.in>
2017-11-18 16:20:49 +0530

    homePage.java : resume function added
    resume capability added in homePage.java

--- homePage.java
+++ homePage.java
@@ -194,6 +194,11 @@ public class homePage extends Application
       };
 
       newGameBut.addEventFilter(MouseEvent.MOUSE_CLICKED, gameHandler);
+      
+      String path = System.getProperty("user.dir");
+      String[] gameList = (new File(path + "/SavedGames")).list();
+      resumeBut.setOnAction(new resumeHandler(stage, gameList[gameList.length-1]));
+
    } 
    /**
         * Launches the game interface
